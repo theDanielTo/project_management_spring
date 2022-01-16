56578ac8b2c8c4fb560aa6114e03f1e5d6eda516
theDanielTo <dto1989@gmail.com>
2022-01-16 14:47:49 -0800

    Add ProjectRepository interface. Fix /save redirect.

--- src/main/java/com/dto/pma/controllers/ProjectController.java
+++ src/main/java/com/dto/pma/controllers/ProjectController.java
@@ -1,7 +1,9 @@
 package com.dto.pma.controllers;
 
+import com.dto.pma.dao.ProjectRepository;
 import com.dto.pma.entities.Project;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
+
+  @Autowired
+  ProjectRepository proRepo;
 
   @GetMapping("/new")
   public String displayProjectForm(Model model) {
@@ -23,8 +28,10 @@ public class ProjectController {
   }
 
   @PostMapping("/save")
-  public String createProject(Model model) {
-    // this should handle saving to the DB
-    return "";
+  public String createProject(Project project, Model model) {
+    proRepo.save(project);
+
+    // use a redirect to prevent duplicate submissions
+    return "redirect:/projects/new";
   }
 }
---
+++ src/main/java/com/dto/pma/dao/ProjectRepository.java
@@ -0,0 +1,9 @@
+package com.dto.pma.dao;
+
+import com.dto.pma.entities.Project;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+}
