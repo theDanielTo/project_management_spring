94e9e9e47639b4a64e16db34252d36a4456b0302
theDanielTo <dto1989@gmail.com>
2022-01-16 13:44:52 -0800

    first commit

---
+++ src/main/java/com/dto/pma/projectmanagementspring/ProjectManagementSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dto.pma.projectmanagementspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dto/pma/projectmanagementspring/ProjectManagementSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dto.pma.projectmanagementspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
