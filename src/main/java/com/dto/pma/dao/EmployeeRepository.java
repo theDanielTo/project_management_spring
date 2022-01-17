70b656c76e51d5f9550ca9818c0c131a3940f8f1
theDanielTo <dto1989@gmail.com>
2022-01-16 16:06:47 -0800

    Create home page to display projects & employees

--- src/main/java/com/dto/pma/dao/EmployeeRepository.java
+++ src/main/java/com/dto/pma/dao/EmployeeRepository.java
@@ -1,9 +1,12 @@
 package com.dto.pma.dao;
 
+import java.util.List;
+
 import com.dto.pma.entities.Employee;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-
+  @Override
+  List<Employee> findAll();
 }
