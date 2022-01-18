0b0e0801bea09ca9a9075d0c3c2ff0c23b4cbe83
theDanielTo <dto1989@gmail.com>
2022-01-17 22:11:38 -0800

    Configured chart. Styled home page with bootstrap.

--- src/main/resources/static/js/myChart.js
+++ src/main/resources/static/js/myChart.js
@@ -1,5 +1,29 @@
-let myPieChart = new Chart(ctx, {
+new Chart(document.getElementById('myPieChart'), {
   type: 'pie',
-  data: data,
-  oprtions: options
+  data: {
+    labels: ['Jan', 'Feb', 'Mar'],
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: ['#3e95ccd', '#8e5ea2', '#3cba9f'],
+      borderColor: 'rgb(255, 99, 132)',
+      data: [15, 10, 5],
+    }]
+  },
+  oprtions: {
+    animations: {
+      tension: {
+        duration: 1000,
+        easing: 'linear',
+        from: 1,
+        to: 0,
+        loop: true
+      }
+    },
+    scales: {
+      y: { // defining min and max so hiding the dataset does not change scale range
+        min: 0,
+        max: 100
+      }
+    }
+  }
 });
