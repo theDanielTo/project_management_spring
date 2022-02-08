c766b6141bb73ad7392720a886a5bc4b9d72e213
theDanielTo <dto1989@gmail.com>
2022-02-08 11:40:53 -0800

    Replace DataJpaTest with SpringBootTest

--- src/test/java/com/dto/pma/dao/ProjectRepositoryIntegrationTest.java
+++ src/test/java/com/dto/pma/dao/ProjectRepositoryIntegrationTest.java
@@ -1,4 +1,4 @@
-package com.dto.dao;
+package com.dto.pma.dao;
 
 import com.dto.pma.ProjectManagementSpringApplication;
 import com.dto.pma.dao.ProjectRepository;
@@ -6,7 +6,7 @@ import com.dto.pma.entities.Project;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
@@ -14,9 +14,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
+@SpringBootTest
 @ContextConfiguration(classes = ProjectManagementSpringApplication.class)
 @RunWith(SpringRunner.class)
-@DataJpaTest
 @SqlGroup({
         @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
                 scripts = {"classpath:schema.sql", "classpath:data.sql"}), // load schema.sql & data.sql before execution
