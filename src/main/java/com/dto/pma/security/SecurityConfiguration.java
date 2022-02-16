ef85615eb0eb40f7d86870deb7ec17a326e1234c
theDanielTo <dto1989@gmail.com>
2022-02-16 13:54:12 -0800

    jdbc authentication & authorization

--- src/main/java/com/dto/pma/security/SecurityConfiguration.java
+++ src/main/java/com/dto/pma/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.dto.pma.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,25 +10,37 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     // Authentication
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("myuser")
-                    .password("pass")
-                    .roles("USER")
-                .and()
-                .withUser("daniel")
-                    .password("pass2")
-                    .roles("USER")
-                .and()
-                .withUser("managerUser")
-                    .password("pass3")
-                    .roles("ADMIN");
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "SELECT username, password, enabled " +
+                        "FROM users WHERE username = ?")
+                .authoritiesByUsernameQuery(
+                        "SELECT username, authority " +
+                        "FROM authorities WHERE username = ?");
+//                .withDefaultSchema()
+//                .withUser("myuser")
+//                    .password("pass")
+//                    .roles("USER")
+//                .and()
+//                .withUser("daniel")
+//                    .password("pass2")
+//                    .roles("USER")
+//                .and()
+//                .withUser("managerUser")
+//                    .password("pass3")
+//                    .roles("ADMIN");
     }
 
     @Bean
@@ -41,6 +54,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/projects/new").hasRole("ADMIN")
                 .antMatchers("/employees/new").hasRole("ADMIN")
+                .antMatchers("/h2_console/**").permitAll()
                 .antMatchers("/").authenticated().and().formLogin();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
     }
 }
